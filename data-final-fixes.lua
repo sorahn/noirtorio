76ed5abe904703e19267cbfd5f2303111e216938
andrewreds <andrewreds@gmail.com>
2020-10-05 19:51:17 +1100

    Add desaturation to map

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -86,3 +86,57 @@ end
 if not(next(config.data) == nil) then
 	checkData({})
 end
+
+
+
+
+-- desaturate the map
+local function desaturate(c, sat, bri)
+	-- colors can be either named, on indexed. They also can be valued [0-1] or [0-255], but that doesn't matter for this maths
+	r = c.r or c[1]
+	g = c.g or c[2]
+	b = c.b or c[3]
+	a = c.a or c[4] or 1
+
+	-- from: https://www.w3.org/TR/filter-effects-1/#feColorMatrixElement
+	ret = {
+		r = (r*(0.213 + 0.787*sat) + g*(0.715 - 0.715*sat) + b*(0.072 - 0.072*sat)) * bri,
+		g = (r*(0.213 - 0.213*sat) + g*(0.715 + 0.285*sat) + b*(0.072 - 0.072*sat)) * bri,
+		b = (r*(0.213 - 0.213*sat) + g*(0.715 - 0.715*sat) + b*(0.072 + 0.928*sat)) * bri,
+		a = a,
+	}
+
+	return ret
+end
+
+
+for entity_group_name, entity_group in pairs(data.raw) do
+	for _, entity in pairs(entity_group) do
+		if entity.map_color ~= nil then
+			entity.map_color = desaturate(entity.map_color, 0.1, 0.7)
+		end
+
+		if entity.friendly_map_color ~= nil then
+			entity.friendly_map_color = desaturate(entity.friendly_map_color, 0.1, 0.7)
+		end
+
+		if entity.enemy_map_color ~= nil then
+			entity.enemy_map_color = desaturate(entity.enemy_map_color, 0.1, 0.7)
+		end
+	end
+end
+
+
+-- There are a bunch of default colors in UtilityConstants.chart for the map that we must desaturate too
+local function desaturate_table(t, sat, bri, postfix)
+	for k, v in pairs(t) do
+		if postfix == nil or k:sub(-#postfix) == postfix then
+			t[k] = desaturate(t[k], sat, bri)
+		end
+	end
+end
+desaturate_table(data.raw["utility-constants"].default.chart, 0.1, 0.7, "_color")
+desaturate_table(data.raw["utility-constants"].default.chart.default_color_by_type, 0.1, 0.7)
+desaturate_table(data.raw["utility-constants"].default.chart.default_friendly_color_by_type, 0.1, 0.7)
+
+
