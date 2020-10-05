659363f90effa0be42a076af8b1deafc81417a9b
Hornwitser <github@hornwitser.no>
2020-10-05 18:41:40 +0200

    Pass on exception raised during remote execution
    Gather up all the futures created and make sure if one of them throws
    that the exception is propogated out.

--- run-conversion.py
+++ run-conversion.py
@@ -2,7 +2,7 @@
 
 from PIL import Image, ImageEnhance
 from pathlib import Path
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import FIRST_EXCEPTION, ProcessPoolExecutor, wait
 import os
 import shutil
 import time
@@ -205,31 +205,49 @@ def main():
     start = time.perf_counter()
 
     processor = ProcessPoolExecutor(NUM_PROCESSES)
+    futures = []
+
+    def render(*args):
+        futures.append(processor.submit(render_image, *args))
 
     for filename in generate_filenames(CORE, CORE_EXCLUDE):
-        processor.submit(render_image, filename, 0.7, 0.1)
+        render(filename, 0.7, 0.1)
 
     # Misc STuff
     for filename in generate_filenames(MISC_STUFF):
-        processor.submit(render_image, filename, 0.6, 0.05)
+        render(filename, 0.6, 0.05)
 
     # Base Entities
     for filename in generate_filenames(BASE_ENTITIES, ENTITY_EXCLUDE):
-        processor.submit(render_image, filename, 0.7, 0.05)
+        render(filename, 0.7, 0.05)
 
     # Entites that need more color
     for filename in generate_filenames(BRIGHT_ENTITIES, ENTITY_EXCLUDE):
-        processor.submit(render_image, filename, 0.7, 0.10)
+        render(filename, 0.7, 0.10)
 
     # Terrain
     for filename in generate_filenames(TERRAIN, TERRAIN_EXCLUDE):
-        processor.submit(render_image, filename, 1, 0.4)
+        render(filename, 1, 0.4)
 
     # Ore
     for filename in generate_filenames(ORE, ORE_EXCLUDE):
-        processor.submit(render_image, filename, 0.7, 0.2)
+        render(filename, 0.7, 0.2)
 
+    # Wait for the all tasks to complete or the first one to raise an exception
+    result = wait(futures, return_when=FIRST_EXCEPTION)
+
+    # Cancel pending tasks after one failed with an exception
+    for pending in result.not_done:
+        pending.cancel()
+
+    # Wait for processor to complete all pending tasks that could not be canceled.
     processor.shutdown()
+
+    # Retrieve result for all tasks, this will re-raise the exception if the
+    # task failed and cause it to be printed to the console
+    for done in result.done:
+        done.result()
+
     print(f"Done in {time.perf_counter() - start:.1f}s")
 
 
